2e38e24e79f12f1f01215f67360d4320dcbf4281
Daniel Belokon <daniel.belokon@gmail.com>
2021-12-23 20:10:50 +0200

    initial implementation

--- src/part2/matamikya.c
+++ src/part2/matamikya.c
@@ -1,38 +1,62 @@
+#include <stdlib.h>
 #include "matamikya.h"
 #include "amount_set.h"
+#include "matamikya_order.h"
 #include "set.h"
+#include "matamikya_product.h"
+#include "matamikya_print.h"
 
 struct Matamikya_t
 {
-    int id;
-    AmountSet products;
-    AmountSet product_profits; // by ID
+    Set products;
     Set orders;
+    int order_index;
 };
 
-struct Order_t
+Product getProductById(Matamikya matamikya, int id)
 {
-    unsigned int id;
-    AmountSet product_ids; // by ID
-} * Order;
+    SET_FOREACH(Product, Product, matamikya->products)
+    {
+        if (productGetId(Product) == id)
+            return Product;
+    }
 
-struct Product_t
+    return NULL;
+}
+
+Order getOrderById(Matamikya matamikya, int id)
 {
-    unsigned int id;
-    char *name;
-    MatamikyaAmountType amountType;
-    MtmProductData customData;
-    MtmCopyData copyData;
-    MtmFreeData freeData;
-    MtmGetProductPrice prodPrice;
-} * Product;
+    SET_FOREACH(Order, order, matamikya->orders)
+    {
+        if (orderGetId(order) == id)
+            return order;
+    }
+
+    return NULL;
+}
 
 Matamikya matamikyaCreate()
 {
+    Matamikya new_matamikya = malloc(sizeof(*new_matamikya));
+    if (new_matamikya == NULL)
+        return NULL;
+
+    // TODO: Check for failures
+    new_matamikya->orders = setCreate(orderCopy, orderDelete, orderCompare);
+    new_matamikya->products = setCreate(productCopy, productDelete, productCompare);
+
+    return MATAMIKYA_SUCCESS;
 }
 
 void matamikyaDestroy(Matamikya matamikya)
 {
+    if (matamikya == NULL)
+        return;
+
+    setDestroy(matamikya->products);
+    setDestroy(matamikya->orders);
+
+    return;
 }
 
 MatamikyaResult mtmNewProduct(Matamikya matamikya, const unsigned int id, const char *name,
@@ -40,48 +64,152 @@ MatamikyaResult mtmNewProduct(Matamikya matamikya, const unsigned int id, const 
                               const MtmProductData customData, MtmCopyData copyData,
                               MtmFreeData freeData, MtmGetProductPrice prodPrice)
 {
+    if (matamikya == NULL)
+        return MATAMIKYA_NULL_ARGUMENT;
+
+    if (getProductById(matamikya, id))
+        return MATAMIKYA_PRODUCT_ALREADY_EXIST;
+
+    Product new_product = productCreate(id,
+                                        name,
+                                        amount,
+                                        amountType,
+                                        customData, copyData, freeData,
+                                        prodPrice);
+    if (new_product == NULL)
+        return MATAMIKYA_OUT_OF_MEMORY;
+    setAdd(matamikya->products, new_product);
+
+    // inefficient but w/e, I'll deal with it later
+    productDelete(new_product);
+
+    return MATAMIKYA_SUCCESS;
 }
 
 MatamikyaResult mtmChangeProductAmount(Matamikya matamikya, const unsigned int id, const double amount)
 {
+    if (matamikya == NULL)
+        return MATAMIKYA_NULL_ARGUMENT;
+    Product product;
+
+    if ((product = getProductById(matamikya, id)) == NULL)
+        return MATAMIKYA_PRODUCT_NOT_EXIST;
+
+    if (product->amount < amount)
+        return MATAMIKYA_INSUFFICIENT_AMOUNT;
+
+    product->amount += amount;
+
+    return MATAMIKYA_SUCCESS;
 }
 
 MatamikyaResult mtmClearProduct(Matamikya matamikya, const unsigned int id)
 {
+    if (matamikya == NULL)
+        return MATAMIKYA_NULL_ARGUMENT;
+    Product product;
+
+    if ((product = getProductById(matamikya, id)) == NULL)
+        return MATAMIKYA_PRODUCT_NOT_EXIST;
+
+    if (!setRemove(matamikya->products, product))
+        return -1;
+
+    return MATAMIKYA_SUCCESS;
 }
 
 MatamikyaResult mtmPrintBestSelling(Matamikya matamikya, FILE *output)
 {
+    if (matamikya == NULL)
+        return MATAMIKYA_NULL_ARGUMENT;
+
+    int max_profits = 0;
+    Product profitable_product = NULL;
+
+    SET_FOREACH(Product, product, matamikya->products)
+    {
+        int cur_product_profits = 0;
+        if (max_profits < (cur_product_profits = productGetProfit(product)))
+        {
+            max_profits = cur_product_profits;
+            profitable_product = product;
+        }
+    }
+
+    mtmPrintProductDetails(profitable_product->name, profitable_product->id, profitable_product->amount, 0, output);
+
+    return MATAMIKYA_SUCCESS;
 }
 
 // ORDERS
 
 unsigned int mtmCreateNewOrder(Matamikya matamikya)
 {
+    if (matamikya == NULL)
+        return 0;
+
+    int index = matamikya->order_index++;
+    Order order = orderCreate(index);
+    setAdd(matamikya->orders, order);
+
+    orderDelete(order);
+
+    return index;
 }
 
 MatamikyaResult mtmChangeProductAmountInOrder(Matamikya matamikya, const unsigned int orderId,
                                               const unsigned int productId, const double amount)
 {
-}
+    if (matamikya == NULL)
+        return MATAMIKYA_NULL_ARGUMENT;
 
-MatamikyaResult mtmChangeProductAmountInOrder(Matamikya matamikya, const unsigned int orderId,
-                                              const unsigned int productId, const double amount)
-{
+    Order order = getOrderById(matamikya, orderId);
+
+    if (order == NULL)
+        return MATAMIKYA_ORDER_NOT_EXIST;
+
+    orderChangeItemAmount(order, productId, amount);
+
+    return MATAMIKYA_SUCCESS;
 }
 
 MatamikyaResult mtmShipOrder(Matamikya matamikya, const unsigned int orderId)
 {
+    if (matamikya == NULL)
+        return MATAMIKYA_NULL_ARGUMENT;
+
+    Order order = getOrderById(matamikya, orderId);
+
+    if (order == NULL)
+        return MATAMIKYA_ORDER_NOT_EXIST;
+
+    // TODO: Keep warehouse unchanged if operation fails
+    AS_FOREACH(int *, product_id, order->product_ids)
+    {
+        double amount_out = 0;
+        asGetAmount(order->product_ids, product_id, &amount_out);
+        mtmChangeProductAmount(matamikya, *product_id, amount_out);
+    }
+
+    return MATAMIKYA_SUCCESS;
 }
 
 MatamikyaResult mtmCancelOrder(Matamikya matamikya, const unsigned int orderId)
 {
+    if (matamikya == NULL)
+        return MATAMIKYA_NULL_ARGUMENT;
+
+    Order order = getOrderById(matamikya, orderId);
+
+    if (order == NULL)
+        return MATAMIKYA_ORDER_NOT_EXIST;
+
+    setRemove(matamikya->orders, order);
+
+    return MATAMIKYA_SUCCESS;
 }
 
 MatamikyaResult mtmPrintOrder(Matamikya matamikya, const unsigned int orderId, FILE *output)
 {
-}
-
-MatamikyaResult mtmPrintBestSelling(Matamikya matamikya, FILE *output)
-{
+    return MATAMIKYA_SUCCESS;
 }
